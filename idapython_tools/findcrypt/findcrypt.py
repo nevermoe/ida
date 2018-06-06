b7be2ee7e6953a40f5e8535347561ef7e22a0f59
nevermoe <nevermoe@users.noreply.github.com>
2018-06-06 14:09:43 +0900

    fix ida api

--- idapython_tools/findcrypt/findcrypt.py
+++ idapython_tools/findcrypt/findcrypt.py
@@ -26,49 +26,51 @@ def main():
         const["byte_array"] = convert_to_byte_array(const)
 
     for start in idautils.Segments():
-        print("[*] searching for crypto constants in %s" % idc.get_segm_name(start))
+        print("[*] searching for crypto constants in %s" % idc.SegName(start))
+
         ea = start
-        while ea < idc.get_segm_end(start):
-            bbbb = list(struct.unpack("BBBB", idc.get_bytes(ea, 4)))
+        while ea < idc.SegEnd(start):
+            bbbb = list(struct.unpack("BBBB", idc.GetManyBytes(ea, 4) or "AAAA"))
             for const in non_sparse_consts:
                 if bbbb != const["byte_array"][:4]:
                     continue
-                if map(lambda x:ord(x), get_bytes(ea, len(const["byte_array"]))) == const["byte_array"]:
+                if map(lambda x:ord(x), idc.GetManyBytes(ea, len(const["byte_array"])) or list() ) == const["byte_array"]:
                     print(("0x%0" + str(digits) + "X: found const array %s (used in %s)") % (ea, const["name"], const["algorithm"]))
-                    idc.set_name(ea, const["name"])
+                    idc.MakeName(ea, const["name"])
                     if const["size"] == "B":
-                        idc.create_byte(ea)
+                        idc.MakeByte(ea)
                     elif const["size"] == "L":
-                        idc.create_dword(ea)
+                        idc.MakeDword(ea)
                     elif const["size"] == "Q":
-                        idc.create_qword(ea)
-                    make_array(ea, len(const["array"]))
+                        idc.MakeQword(ea)
+                    MakeArray(ea, len(const["array"]))
                     ea += len(const["byte_array"]) - 4
                     break
             ea += 4
+            #print "ea: 0x%x " % ea
 
         ea = start
-        if get_segm_attr(ea, SEGATTR_TYPE) == 2:
-            while ea < get_segm_end(start):
-                d = ida_bytes.get_dword(ea)
+        if idc.GetSegmentAttr(ea, SEGATTR_TYPE) == 2:
+            while ea < idc.SegEnd(start):
+                d = idc.Dword(ea)
                 for const in sparse_consts:
                     if d != const["array"][0]:
                         continue
                     tmp = ea + 4
                     for val in const["array"][1:]:
                         for i in range(8):
-                            if ida_bytes.get_dword(tmp + i) == val:
+                            if idc.Dword(tmp + i) == val:
                                 tmp = tmp + i + 4
                                 break
                         else:
                             break
                     else:
                         print(("0x%0" + str(digits) + "X: found sparse constants for %s") % (ea, const["algorithm"]))
-                        cmt = idc.get_cmt(idc.prev_head(ea), 0)
+                        cmt = idc.GetCommentEx(idc.prev_head(ea), 0)
                         if cmt:
-                            idc.set_cmt(idc.prev_head(ea), cmt + ' ' + const["name"], 0)
+                            idc.CommentEx(idc.prev_head(ea), cmt + ' ' + const["name"], 0)
                         else:
-                            idc.set_cmt(idc.prev_head(ea), const["name"], 0)
+                            idc.CommentEx(idc.prev_head(ea), const["name"], 0)
                         ea = tmp
                         break
                 ea += 1
